be9e7d64d97b54931fc4a596ca7b9e5de25304f3
Sooyoung Jung <91307997+syj7421@users.noreply.github.com>
2024-05-17 22:31:28 +1000

    create skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,12 +1,35 @@
 import React from "react";
 import "./Skills.css";
 import SkillsSection from "./SkillsSection";
+const frontEndSkills = [
+{
+  name: 'Javascipt',
+  loc: require("../Assets/Images/JavaScript-logo.png"),
+},{
+  name: 'Typescript',
+  loc: require("../Assets/Images/typescript-logo.png"),
+},{
+  name: 'React',
+  loc: require("../Assets/Images/react-logo.png"),
+}
+];
+const backEndSkills = [{
+  name: 'Node.js',
+  loc: require("../Assets/Images/node-logo.png"),
+},{
+  name: 'Python',
+  loc: require("../Assets/Images/python-logo.jpg"),
+},];
+const otherSkills = [{
+  name: 'C',
+  loc: require("../Assets/Images/c-logo.png"),
+}];
 
 function Skills(){
   return <div className="skills">
-    <SkillsSection title="Front-end"/>
-    <SkillsSection title="Back-end"/>
-    <SkillsSection title="Others"/>
+    <SkillsSection title="Front-end" skillsList={frontEndSkills}/>
+    <SkillsSection title="Back-end" skillsList={backEndSkills}/>
+    <SkillsSection title="Others" skillsList={otherSkills}/>
   </div>
 }
 
--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import "./SkillsSection.css";
 
 function SkillsSection(props){
   return  <div className="skills-section">
     <h2>{props.title}</h2>
-    <div></div>
+    <div>
+      {props.skillsList.map((skill) => {
+        return <div key={skill.name} className="skill-container"><img src={skill.loc} alt={skill.name}/></div>
+      })}
+
+    </div>
 </div>
 }
 
