e6045d813be1040f60b93fbc5983ed7944829037
Sooyoung Jung <91307997+syj7421@users.noreply.github.com>
2024-05-20 01:22:20 +1000

    create about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,43 @@
 import React from "react";
 import "./About.css";
 
-function About(){
-  return <div className="about" id="about">
-      <div className="about-title">About Me</div>
-      <div className="about-content">
-        <h2>노력하는 사람</h2>
-        <p>어릴때 부터 호기심이 많은 성격에 만들어 보았던 간단한 계산기 프로그램으로 무언가를 만드는 것의 재미를 느끼게 되었고 그때부터 개발자를 꿈꾸게 되었습니다.
-계속해서 완성도 높은 코드, 그리고 완성도 높은 UI를 위해 매일 고뇌하는 사람이 되도록 하겠습니다.
-잘부탁드립니다.</p>
+const skills = [
+  { skill: "CSS", level: 80 },
+  { skill: "HTML", level: 75 },
+  { skill: "React", level: 70 },
+  { skill: "JavaScript", level: 85 },
+  { skill: "NextJS", level: 60 },
+  { skill: "Node.js", level: 65 },
+  { skill: "Ruby/Rails", level: 50 },
+  { skill: "Python", level: 55 },
+  { skill: "Figma", level: 45 },
+  { skill: "Illustrator", level: 40 }
+];
+
+function About() {
+  return <div id="about">
+    <h1 className="about-header">ABOUT</h1>
+    <div className="bold-line">-</div>
+    <div className="profile-skills-container">
+      <div className="profile-section">
+        <img src={require('../Assets/Images/github-logo.png')} alt="Profile"/>
+        <p className="profile-title">Who's this guy?</p>
+        <p className="profile-content">I'm a Front-End Developer for ChowNow in Los Angeles, CA.
+          I have serious passion for UI effects, animations and creating intuitive, dynamic user experiences.</p>
+      </div>
+      <div className="skill-charts">
+        {skills.map(skill => (
+          <div className="skill-chart" key={skill.skill}>
+            <div className="label">{skill.skill}</div>
+            <div className="skill-bar">
+              <div className="skill-level" style={{'--skill-level': `${skill.level}%`}}></div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
-
+  </div>
 }
+
 
 export default About;
